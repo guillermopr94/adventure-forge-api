bfe8c3981dc8fd0ea9105270bb423a264b7eb137
Guillermo Pérez Ruiz <guillermoperezruiz94@gmail.com>
2025-12-12 13:22:19 +0100

    feat: Initialize NestJS application in `main.ts` and add `start:prod` script.

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ dotenv.config();
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors(); // Allow all CORS for now, can restrict later
-    await app.listen(3001);
-    console.log('Application is running on: http://localhost:3001');
+    await app.listen(process.env.PORT || 3001);
+    console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
